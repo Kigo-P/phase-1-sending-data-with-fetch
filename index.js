9e1a5c570fac5f28b7a8fdb8e191f2031c3623f5
Kigo-P <peterkigo33@gmail.com>
2024-07-22 13:35:47 +0300

    last Commit

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Add your code here
+function submitData(name,email){
+    // let userEmail = "peterkigo33@gmail.com"
+    // let userName = "peter"
+    let userDetails = {
+        name: name,
+        email: email
+    }
+  return fetch("http://localhost:3000/users",{
+    method: "POST",
+    headers: {
+        "Content-Type": "application/json",
+        "Accept" : "application/json"
+    },
+    body: JSON.stringify(userDetails)
+    })
+    .then(response => response.json())
+    .then(json => {
+        // AppendING the ID to the DOM
+        const body = document.querySelector('body');
+        const p = document.createElement('p');
+        p.textContent = json.id;
+        body.appendChild(p);
+    })
+    .catch (error =>{
+        // Handle errors and append the error message to the DOM
+      const body = document.querySelector('body');
+      const p = document.createElement('p');
+      p.textContent = error.message;
+      body.appendChild(p);
+    })
+}
+submitData("peter","peterkigo33@gmail.com")
